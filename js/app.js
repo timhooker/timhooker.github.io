80588191fc737f6318e4213d6eddfc32d623867b
Tim Hooker <timhooker@me.com>
2015-11-10 23:37:59 -0500

    changes to portfolio

---
+++ js/app.js
@@ -0,0 +1,2 @@
+$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")||location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html,body").animate({scrollTop:t.offset().top},380),!1}})});
+//# sourceMappingURL=app.js.map
